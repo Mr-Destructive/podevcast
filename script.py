0c996faa7fa24f7333912e28791972da8912f8be
Mr-Destructive <gormeet711@gmail.com>
2022-02-10 22:25:39 +0530

    added list page

--- script.py
+++ script.py
@@ -4,21 +4,44 @@ import feedparser
 import os
 
 template_env = Environment(loader=FileSystemLoader(searchpath='./layouts/'))
-feed = feedparser.parse('https://feeds.pacific-content.com/commandlineheroes')
-
-index_template = template_env.get_template('index.html')
-
+feed_list = ['https://feeds.pacific-content.com/commandlineheroes',
+        'https://www.pythonpodcast.com/feed/mp3/',
+        'https://talkpython.fm/episodes/rss',
+        'https://realpython.com/podcasts/rpp/feed',
+        'https://feeds.simplecast.com/l_5sU3vk']
 ep=[]
 audiofiles = []
 ep_title = []
-for i in range(0,len(feed['entries'])):
-    ep_title=(feed['entries'][i]['title'])
-    audiofiles=(feed['entries'][i]['links'][1]['href'])
-    
-    obj = {}
-    obj['title'] = ep_title
-    obj['audiolink'] = audiofiles
-    ep.append(obj)
+pod_title = []
+pod_list = []
+for feed_link in feed_list:
+    pod_obj = {}
+    feed = feedparser.parse(feed_link)
+    #https://feeds.pacific-content.com/commandlineheroes
+
+
+    index_template = template_env.get_template('index.html')
+    list_template = template_env.get_template('list.html')
+
+    for i in range(0,len(feed['entries'])):
+        ep_title = feed['entries'][i]['title']
+        audiofiles = feed['entries'][i]['links'][1]['href']
+        if(feed['entries'][i].has_key('image')):
+            cover_image = feed['entries'][i]['image']['href']
+        else:
+            cover_image = feed['feed']['image']
+
+        obj = {}
+
+        obj['title'] = ep_title
+        obj['audiolink'] = audiofiles
+        obj['cover'] = cover_image
+        if(i==0):    
+            ep.append(obj)
+    pod_obj['title'] = feed['feed']['title']
+    pod_obj['cover'] = feed['feed']['image']['href']
+    pod_list.append(pod_obj)
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 output_dir = os.path.join(BASE_DIR, 'site')
@@ -31,4 +54,12 @@ with open(os.path.join('site/index.html'), 'w') as index_file:
             ep=ep,
         )
     )
+
+with open(os.path.join('site/list.html'), 'w') as list_file:
+    list_file.write(
+        list_template.render(
+            ep=pod_list,
+        )
+    )
+os.system('cp -r static site')
 #os.system('python -m http.server -d site')
